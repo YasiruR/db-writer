491c56df7e1927dadcd29a7e8e58091915546bf3
Yasi <wyasirurathsara@gmail.com>
2022-03-12 13:24:12 +0100

    :heavy_check_mark: minor fix in neo4j

--- neo4j/client.go
+++ neo4j/client.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/YasiruR/db-writer/generic"
 	goNeo4j "github.com/neo4j/neo4j-go-driver/v4/neo4j"
-	"log"
+	"sync"
 	"sync/atomic"
 )
 
@@ -21,31 +21,41 @@ func Client() generic.Database {
 }
 
 func (n *neo4j) Init(cfg generic.DBConfigs) generic.Database {
-	db, err := goNeo4j.NewDriver(cfg.Addr, goNeo4j.BasicAuth(cfg.Username, cfg.Passwd, ``))
+	db, err := goNeo4j.NewDriver(`bolt://`+cfg.Addr, goNeo4j.BasicAuth(cfg.Username, cfg.Passwd, ``))
 	if err != nil {
-		log.Fatalln(err)
+		generic.Fatal(err)
 	}
 
 	n.db = db
 	return n
 }
 
-func (n *neo4j) Write(fields []string, values [][]string, _ generic.DataConfigs) {
-	var failed uint64
+func (n *neo4j) Write(fields []string, values [][]string, dataCfg generic.DataConfigs) {
+	wg := &sync.WaitGroup{}
+	var success uint64
 	n.setTx(fields)
 
-	for _, val := range values {
+	for i, val := range values {
+		if dataCfg.Limit >= 0 && dataCfg.Limit == i {
+			break
+		}
+
+		wg.Add(1)
 		go func(val []string) {
+			defer wg.Done()
 			session := n.db.NewSession(goNeo4j.SessionConfig{})
 			defer session.Close()
 			go n.sendParams(fields, val)
 
 			_, err := session.WriteTransaction(n.insertFunc)
-			if err != nil {
-				atomic.AddUint64(&failed, 1)
+			if err == nil {
+				atomic.AddUint64(&success, 1)
 			}
 		}(val)
 	}
+
+	wg.Wait()
+	fmt.Println(`total writes (redis): `, int(success))
 }
 
 func (n *neo4j) setTx(fields []string) {
